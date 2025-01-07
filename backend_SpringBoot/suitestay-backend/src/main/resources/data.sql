8a69b5084d9e95db2981a3d59c5350be279dd97f
Angel <angelreyes968@icloud.com>
2025-01-06 22:25:19 -0800

    test

--- backend_SpringBoot/suitestay-backend/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT IGNORE INTO rooms (room_Type, room_Dimension, bed_Type, max_Guest, room_Price, room_image) VALUES
-('Suite Stay Resort King', '745 sq. ft.', '1 King Bed', 'Max Guests 2', 120.20, '/assets/rooms/resort_king.jpg'),
-('Suite Stay Resort Two Queens', '745 sq. ft.', '2 Queen Beds', 'Max Guests 4', 120.20, '/assets/rooms/resort_two_queen.jpg'),
-('Panoramic View Suite King', '825 sq. ft.', '1 Kind Bed', 'Max Guests 3', 200.50, '/assets/rooms/panoramic_king.jpg'),
-('Panoramic View Suite Two Queens', '825 sq. ft.', '2 Queen Beds', 'Max Guests 4', 200.50, '/assets/rooms/panoramic_two_queen.jpg'),
-('Suite Stay Penthouse', '14650 sq. ft.', '1 King Beds', 'Max Guests 6', 950.25, '/assets/rooms/penthouse.jpg'),
-('Suite Stay Two Bedroom Penthouse', '2060 sq. ft.', '2 King Beds', 'Max Guests 8', 1540.30, '/assets/rooms/two_bedroom_penthouse.jpg');
