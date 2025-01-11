6b2a2379d1cef01382123901cbce5d0bf697a83f
Angel <angelreyes968@icloud.com>
2025-01-11 15:01:44 -0800

    added docker file to backend

--- SuiteStay/backend_SpringBoot/suitestay-backend/src/test/java/com/suitestay/suitestay_backend/SuiteStayBackendApplicationTests.java
+++ SuiteStay/backend_SpringBoot/suitestay-backend/src/test/java/com/suitestay/suitestay_backend/SuiteStayBackendApplicationTests.java
@@ -1,9 +1,10 @@
 package com.suitestay.suitestay_backend;
 
+import dev.Reyes.SuiteStayBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SuiteStayBackendApplication.class)
 class SuiteStayBackendApplicationTests {
 
 	@Test
